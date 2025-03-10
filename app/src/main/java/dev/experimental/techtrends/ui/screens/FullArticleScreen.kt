2c826c818549de256cbcb4635c5287ee966a2ac4
fahad <fsadakathussain@gmail.com>
2025-03-10 16:13:05 +0600

    feat: Implement Full Article Screen UI
    This commit introduces the UI for the Full Article Screen, utilizing Jetpack Compose. The screen includes:
    
    -   A close icon (cross_round).
    -   Date and time display.
    -   Article title.
    -   Author information.
    -   Descriptive text content.
    - Spacing and padding.
    - Text styles with appropriate font weights and sizes.

--- app/src/main/java/dev/experimental/techtrends/ui/screens/FullArticleScreen.kt
+++ app/src/main/java/dev/experimental/techtrends/ui/screens/FullArticleScreen.kt
@@ -1,11 +1,74 @@
 package dev.experimental.techtrends.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.experimental.techtrends.R
 
 
 @Composable
 @Preview(showBackground = true)
 fun FullArticleScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.cross_round), contentDescription = "",
+            modifier = Modifier.padding(top = 30.dp)
+        )
 
+        Spacer(Modifier.height(40.dp))
+        Text(
+            "Monday March 10, 2025 at 10:40", style = TextStyle(
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Light
+            )
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            "Weak and the ineffecetive", style = TextStyle(
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
+        Spacer(Modifier.height(10.dp))
+
+        Text(
+            "Author name", style = TextStyle(
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Light
+            )
+        )
+        Text(
+            "Written in", style = TextStyle(
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Light
+            )
+        )
+
+        Spacer(Modifier.height(25.dp))
+
+        Text(
+            "Descriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive TextDescriptive Text",
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal
+            )
+        )
+        Spacer(Modifier.height(30.dp))
+    }
 }
